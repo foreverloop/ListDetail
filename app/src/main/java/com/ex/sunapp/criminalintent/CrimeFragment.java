1ef68aa18566e55dedba5b88eee3ec982f191905
foreverloop <cjones5709@gmail.com>
2016-06-24 20:11:12 +0100

    add widgets and landscape view

--- app/src/main/java/com/ex/sunapp/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/ex/sunapp/criminalintent/CrimeFragment.java
@@ -8,12 +8,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleText;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckbox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,20 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime,container,false);
 
+        mDateButton = (Button) v.findViewById(R.id.crime_date_button);
         mTitleText =  (EditText) v.findViewById(R.id.crime_title);
+        mSolvedCheckbox = (CheckBox) v.findViewById(R.id.crime_solved);
+
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
         mTitleText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
